95e0e9f5554141c1713e9014fd09d3a41a9952a1
seapp88 <seapp88@icloud.com>
2019-07-09 12:19:24 +0300

    Update

--- index.js
+++ index.js
@@ -27,22 +27,22 @@ global.Driver = require('./driver/index.js');
 server.listen(3000, () => {
     console.log('Start server 3000 port');
 
-    // const pythonProcess = spawn('sudo', ["python3", __dirname + "/driver.py", "--port", "3000"], {
-    //     detached: false,
-    //     stdio: 'pipe'
-    // });
-    //
-    // process.on('exit', () => {
-    //     pythonProcess.kill()
-    // });
-    //
-    // pythonProcess.on('error', (code) => {
-    //     console.log('python process error with code ' + code);
-    // });
-    //
-    // pythonProcess.on('exit', (code) => {
-    //     console.log('python process exited with code ' + code);
-    // });
+    const pythonProcess = spawn('sudo', ["python3", __dirname + "/driver.py", "--port", "3000"], {
+        detached: false,
+        stdio: 'pipe'
+    });
+
+    process.on('exit', () => {
+        pythonProcess.kill()
+    });
+
+    pythonProcess.on('error', (code) => {
+        console.log('python process error with code ' + code);
+    });
+
+    pythonProcess.on('exit', (code) => {
+        console.log('python process exited with code ' + code);
+    });
 
 });
 
